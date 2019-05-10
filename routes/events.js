ce2b6dd19bd66f047994dda27b065e7a6e9e9844
Ross Grambo <rossgrambo@asana.com>
2019-05-10 13:39:23 -0700

    fixed crash

--- routes/events.js
+++ routes/events.js
@@ -30,6 +30,9 @@ router.post('/', function(req, res) {
                     return
                 }
                 var messages = JSON.parse(body)["messages"];
+                if (!messages) {
+                    return
+                }
                 var message = messages[0];
 
                 var parrots = ['sad_parrot', 'fast_parrot', 'confused_parrot', 'nyan_parrot', 'upvote_party_parrot', 'slow_parrot', 'dreidel_parrot', 'beer_parrot', 'fieri_parrot', 'shuffle_parrot', 'sirenparrot', 'sassy_parrot', 'matrix_parrot', 'xmas_parrot', 'stable_parrot', 'banana_parrot', 'nyan_parrot', 'explody_parrot', 'thumbs_parrot', 'hd_parrot', 'cop_parrot', 'slow_parrot'];
